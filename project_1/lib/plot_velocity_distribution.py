51938ff1f2e8cbfa0fdfc8cb6f39dc2f530faf7e
alexahs <alexander.harold.sexton@gmail.com>
2020-02-04 11:52:12 +0100

    fixed plotting for vel dist

--- project_1/lib/plot_velocity_distribution.py
+++ project_1/lib/plot_velocity_distribution.py
@@ -5,22 +5,24 @@ from .load_lammps_output import *
 
 
 
-def plot_velocity_distribution(arr, vel_component = 1, n_bins = 20):
+def plot_velocity_distribution(arr, vel_components = [0, 1, 2], n_bins = 20):
 
     n_steps = arr.shape[-1]
-    final_hist, _ = np.histogram(arr[:,vel_component,-1], bins=n_bins)
-    hist_series = np.zeros(arr.shape[-1])
+    labels = [r"$v_x$", r"$v_y$", r"$v_z$"]
+
+    for i in vel_components:
+        final_hist, _ = np.histogram(arr[:,i,-1], bins=n_bins)
+        hist_series = np.zeros(arr.shape[-1])
+        for t in range(n_steps):
+            current_hist, _ = np.histogram(arr[:, i, t], bins=n_bins)
+            hist_series[t] = np.sum(current_hist*final_hist)
+
+        hist_series /= np.sum(final_hist**2)
+        plt.plot(range(n_steps), hist_series, label=labels[i])
 
 
-    for t in range(n_steps):
-        current_hist, _ = np.histogram(arr[:, vel_component, t], bins=n_bins)
-        hist_series[t] = np.sum(current_hist*final_hist)
-
-
-    hist_series /= np.sum(final_hist**2)
-
-    plt.plot(range(n_steps), hist_series)
-    # plt.xlabel()
+    plt.legend()
+    plt.xlabel(r"$t_i$")
     plt.ylabel(r"Normalized inner product of $h_i(t)$")
     plt.show()
 
